2fcc8029070ff7975d51b32c3c3dce6e30e3e146
chasla <66211855+chasla@users.noreply.github.com>
2021-08-12 18:05:00 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: 'usus.us',
-  author: 'Chalsa',
+  author: 'usus',
   email: 'us@usus.us',
   link: 'https://chasla.vercel.app',
   description: 'This gonna be an awesome website.',
